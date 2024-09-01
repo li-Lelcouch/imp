a39fa676856876df31d299e3e17ea4f45f99e7d6
lizh <li_zhonghao@mail.ustc.edu.cn>
2024-09-01 17:10:26 +0800

    2

--- download.py
+++ download.py
@@ -1,3 +1,3 @@
 from huggingface_hub import snapshot_download
 
-snapshot_download(repo_id="liuhaotian/LLaVA-Pretrain", local_dir="autodl-tmp/datasets", local_dir_use_symlinks=False)
+snapshot_download(repo_id="microsoft/phi-2", local_dir="autodl-tmp/datasets/base", local_dir_use_symlinks=False)
